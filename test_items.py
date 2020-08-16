0ad1b2e4b1ede357c8b74697f0ba1537b16f369f
Elderlypython <Dc.dkfcnmvbkbwbb1987>
2020-08-16 15:16:52 +0400

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def is_element_present(browser):
+    try:
+        browser.implicitly_wait(10)
+        browser.find_element_by_css_selector(".btn-add-to-basket")
+        return True
+    except:
+        return False
+
+def test_button_presence(browser):
+    browser.get(link)
+
+    assert is_element_present(browser), "Искомая кнопка отсутствует"
